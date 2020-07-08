8858c213cb84c0629f92e2d2c72db69c3242598f
Matheer0 <alexqixuan@gmail.com>
2020-07-08 11:06:59 -0700

    dynamics

--- planner/dynamics.m
+++ planner/dynamics.m
@@ -1,24 +1,13 @@
-function dz = dynAcc(z, u)	
-	% function computes dynamics
-	% inputs: z = [x;v1;v2]
-	% 		  u = [u1;u2]
+function ddq = dynamics(robot_object, config_parameter, dq, u)
 
-	% Unpack the inputs
-	x = z(1:3,:);   % position
-	v1 = z(4:6,:);  % rates (dynamics)
-	% v2 = z(7:9,:);  % rates (integrator)    %unused
-	u1 = u(1:2,:);  % actuators
-	u2 = u(3:5,:);  % kinematics
-
-	
-
-	% Chain integrators
-	dx = v1;
-	%dv1 = dynQuadRotor(x, u1, p);	% Call to the actual dynamics function
-	dv1 = u2;
-	dv2 = u2;
-
-	% Pack up the outputs
-	dz = [dx; dv1; dv2];
-
+    % Calculate joint accelerations for given configuration with applied external forces
+    robot_object.DataFormat = 'column';
+    
+    config = robot_obj.homeConfiguration;
+    for i = 1:numel(config_parameter)
+        config(i).JointPosition = config_parameter(i);
+    end
+    
+	ddq = forwardDynamics(robot_object, config, dq, u);
+    
 end
