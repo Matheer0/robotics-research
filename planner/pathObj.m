89ee27de1073247f210f180a816eb727a1895180
Matheer0 <alexqixuan@gmail.com>
2020-07-08 11:07:38 -0700

    objective

--- planner/pathObj.m
+++ planner/pathObj.m
@@ -1,24 +1,8 @@
-function [dObj, uStar] = pathObj(u,w)
+function integrand = pathObj(x,u)
 	% function computes the cost function integrand for the trajectory
-	% INPUTS:
-	%   u = [u1;u2] = [actuator; derivative] = [2+3, n]
-	%
-	% OUTPUTS:
-	%   dObj = [1,n] = path objective
-	%   uStar = [3,n] = normalized derivatives
-
-
-	if nargin == 1
- 	   w = [1,1,1];
-	end
-
-	n = size(u,2);
-
-	uStar = zeros(3,n);
-	uStar(1,:) = w(1)*u(3,:);
-	uStar(2,:) = w(2)*u(4,:);
-	uStar(3,:) = w(3)*u(5,:);
-
-	dObj = sum(uStar.^2,1);
-
+	u
+	product = u' * u;
+    integrand = sum(product(:));
+    size(integrand)
+    
 end
