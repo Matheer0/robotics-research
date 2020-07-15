1c34c01161eaf2238518d1c74ecfd44437d6c8b2
Alex Li <55008621+Matheer0@users.noreply.github.com>
2020-07-15 10:54:00 -0700

    Delete pathObj.m

--- planner/pathObj.m
+++
@@ -1,8 +0,0 @@
-function integrand = pathObj(x,u)
-	% function computes the cost function integrand for the trajectory
-	u
-	product = u' * u;
-    integrand = sum(product(:));
-    size(integrand)
-    
-end
