ab824a82c1e0c6815e554426b06e0e63128f2bc0
Matheer0 <alexqixuan@gmail.com>
2020-07-25 17:35:28 -0700

    changed configuration format

--- planner/manipulability.m
+++ planner/manipulability.m
@@ -1,15 +1,11 @@
 function measure = manipulability(robot_obj, config_parameter)
-    % function calculating manipulability 
-    
-    config = robot_obj.homeConfiguration;
-    for i = 1:numel(config_parameter)
-        config(i).JointPosition = config_parameter(i);
-    end
+    % function calculating manipulability   
     
     endEffector = robot_obj.BodyNames{end} ;           % Get end-effector frame name
-    jacobian = robot_obj.geometricJacobian(config, endEffector);
+    jacobian = robot_obj.geometricJacobian(config_parameter, endEffector);
     
     % jacobian = jacobian(4:6,:);           
     determinant = det( jacobian * transpose(jacobian) );
     measure = sqrt(determinant);
+    
 end
