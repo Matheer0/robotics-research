43636cd0e70ffac52a93c1d90adb61c6be86ecca
Matheer0 <alexqixuan@gmail.com>
2020-07-27 23:22:18 -0700

    optimized path generation added

--- iiwa14/main.m
+++ iiwa14/main.m
@@ -1,114 +1,130 @@
 clear all ; close all ;  
 
+%% robot setup
 robot       = importrobot('iiwa14.urdf');
-dim_joint   = numel(robot.homeConfiguration) ; % Get number of joints 
-endEffector = robot.BodyNames{end} ; % Get end-effector frame name
+robot.DataFormat = 'column';
+dim_joint   = numel(robot.homeConfiguration); % Get number of joints 
+endEffector = robot.BodyNames{end}; % Get end-effector frame name
 
 
-%% Specify the some parameters 
+%% specify parameters for robot object
+
+% linear path planning parameters
 time_step = 0.1 ; % time step, in seconds
 hand_speed = 0.1; % m/s
-
-joint_pos_init = robot.randomConfiguration ; 
-hand_pos_init  = getTransform(robot, joint_pos_init, endEffector);  % current hand position given the random joint angles
-cartesian_pos_final = [0.4, 0, 0.6];
-hand_pos_final = trvec2tform(cartesian_pos_final) * axang2tform([0 1 0 pi]); % target hand position
-
-
-%% Generate task-space trajectory
-[taskWaypoints, timeInterval]   = generate_trajectory(hand_pos_init, hand_pos_final, time_step, hand_speed) ; 
-dim_state = dim_joint * 2 ;
-dim_steps = ceil( ( timeInterval(2) - timeInterval(1) ) / time_step ) ;
+                                                                                                                        
+% optimized path planning parameters
+uMax = 50 * ones(dim_joint,1); % random control constraints for path planning
+alpha = 0.9;       % trade-off parameter
 
 
-%% Non-optimized Control Task-Space Motion
-q       = joint_pos_init ;
-q_param = [q(1:dim_joint). JointPosition]';   % 7x1 matrix  
-dq      = zeros(size(q_param)) ;              % 7x1 matrix 
-state   = [q_param', dq'] ;                   % 1x14 matrix
+% control optimization parameters
+max_joint_speed = [0; 1.48; 1.48; 1.75; 1.31; 2.27; 2.36]; % random joint speed constraints, used for 
+                                                           % fmincon in inverse_kinematics_opt.m
 
-manipulability_index        = zeros(1, dim_steps) ;
-trajectory.time             = zeros(1, dim_steps) ;
-trajectory.state            = zeros(dim_steps, dim_state) ;
-trajectory.hand_pos_desired = zeros(dim_steps, 6) ;
-trajectory.hand_pos_current = zeros(dim_steps, 6) ;
+                                                     
+
+%% robot positions setup
+
+%joint_pos_init = robot.randomConfiguration;   % 7x1 matrix
+%joint_pos_final = robot.randomConfiguration;
+
+%cartesian_pos_final = [0.4, 0, 0.6];
+%hand_pos_final = trvec2tform(cartesian_pos_final) * axang2tform([0 1 0 pi]); % target hand position
+
+joint_pos_init = [1.366810854767469; -0.047714618953960; 0.465977092464026;
+                  -1.100463967687917; -0.244195969366768;1.939780737310899;
+                  0.285919865265083];
+joint_pos_final = [-1.34651803292669; 1.35260787526178;  1.11890660499873;
+                  0.435225571215641; -0.670851645972758; -1.81988132007208;
+                  3.04573368893129];
+
+              
+hand_pos_final  = getTransform(robot, joint_pos_final, endEffector); 
+cartesian_pos_final = tform2trvec(hand_pos_final);  % cartesian final postion
 
 
-%% Optimized Control Task-Space Motion
-q_opt       = joint_pos_init ;
-q_opt_param = [q_opt(1:dim_joint). JointPosition]';   % 7x1 matrix   
-dq_opt      = zeros(size(q_opt_param)) ;              % 7x1 matrix 
-state_opt   = [q_opt_param', dq_opt'] ;               % 1x14 matrix
-
-manipulability_index_opt        = zeros(1, dim_steps) ;
-trajectory_opt.time             = zeros(1, dim_steps) ;
-trajectory_opt.state            = zeros(dim_steps, dim_state) ;
-trajectory_opt.hand_pos_desired = zeros(dim_steps, 6) ;
-trajectory_opt.hand_pos_current = zeros(dim_steps, 6) ;
 
 
-%%
-n = 1 ;
-for t = timeInterval(1) : time_step : timeInterval(2)
-    
-    %% Non-opt
-    
-    pose_desired = taskWaypoints(:,:,n) ;                  % desired hand position
-    pose_current = getTransform(robot, q, endEffector) ;   % current hand position 
-    desired_velocity = transformation_diff(pose_current, pose_desired) ; % calculate the velocity needed for the desired hand position
-    [dq, measure] = inverse_kinematics(robot, q, desired_velocity ) ;     % use inverse kinematics to control the robot
-    state = [q_param', dq'] ;
-    
-    % save data for visualization
-    manipulability_index(n) = measure ;
-    trajectory.time(n) = t ;    % save data for visualization  
-    trajectory.hand_pos_desired(n,:) = transformation_diff(pose_desired) ;
-    trajectory.hand_pos_current(n,:) = transformation_diff(pose_current) ;    
-    trajectory.state(n,:) = state ; 
-    % get next state
-    q_param  = q_param + time_step * dq; 
-    for i = 1:dim_joint
-        q(i).JointPosition = q_param(i);
-    end    
-    
-    %% Opt
-   
-    pose_desired_opt = taskWaypoints(:,:,n) ;                  
-    pose_current_opt = getTransform(robot, q_opt, endEffector) ;  
-    desired_velocity_opt = transformation_diff(pose_current_opt, pose_desired_opt) ; 
-    [dq_opt, measure_opt] = inverse_kinematics_opt(robot, q_opt, desired_velocity_opt ) ;   
-    state_opt = [q_opt_param', dq_opt'] ;        
-    
-    manipulability_index_opt(n) = measure_opt ;
-    trajectory_opt.time(n) = t ;    
-    trajectory_opt.hand_pos_desired(n,:) = transformation_diff(pose_desired_opt) ;
-    trajectory_opt.hand_pos_current(n,:) = transformation_diff(pose_current_opt) ;    
-    trajectory_opt.state(n,:) = state_opt ; 
-    
-    % get next state
-    q_opt_param  = q_opt_param + time_step * dq_opt ; 
-    for i = 1:dim_joint
-        q_opt(i).JointPosition = q_opt_param(i);
-    end 
-    
-    %%   
-    n = n + 1 ;   
-end
+%% linear trajectory generation
+[trajectory, trajectory_opt] = linear_traj(robot, joint_pos_init,... 
+                                           hand_pos_final, time_step,... 
+                                           hand_speed, max_joint_speed);
 
-%% Compare two methods
-% Non-opt  Version
-ave = mean(manipulability_index);
-minimum = min(manipulability_index);
-maximum = max(manipulability_index);
-fprintf('Non-Opt: Ave %d, Min %d, Max %d \n', ave, minimum, maximum);
+% non-optimized control
+[ave, minimum, maximum] = checkout_result(trajectory.manipulability_index);
 
-% Opt  Version
-ave_opt = mean(manipulability_index_opt);
-minimum_opt = min(manipulability_index_opt);
-maximum_opt = max(manipulability_index_opt);
-fprintf('Opt: Ave %d, Min %d, Max %d \n', ave_opt, minimum_opt, maximum_opt);
+% optimized control
+[ave_opt, minimum_opt, maximum_opt]...
+    = checkout_result(trajectory_opt.manipulability_index_opt);
 
-visualize_trajectory(robot, trajectory, cartesian_pos_final) ;
-visualize_trajectory(robot, trajectory_opt, cartesian_pos_final) ;
+
+
+%% optimized trajectory generation
+[optTrajectory, optTrajectory_opt] = opt_traj(robot, joint_pos_init,... 
+                                        joint_pos_final, uMax,... 
+                                        alpha, max_joint_speed);
+
+% non-optimized control
+[nonLinear_ave, nonLinear_minimum, nonLinear_maximum]...
+     = checkout_result(optTrajectory.manipulability_index);
+
+% optimized control
+[nonLinear_ave_opt, nonLinear_minimum_opt,nonLinear_maximum_opt] ... 
+    = checkout_result(optTrajectory_opt.manipulability_index_opt);
+
+
+% alpha = 0.1:
+%       Non-Opt: Ave 3.820968e-02, Min 3.796091e-03, Max 9.864981e-02  
+%       Opt Version: Opt: Ave 7.924077e-02, Min 3.293399e-02, Max 1.319185e-01
+
+
+% alpha = 0.5:
+%       Non-Opt: Ave 4.197011e-02, Min 1.151876e-03, Max 1.015073e-01
+%       Opt Version: Ave 8.751646e-02, Min 4.002992e-02, Max 1.464803e-01
+
+
+
+% alpha = 0.8:
+%       Non-Opt: Ave 3.367299e-02, Min 2.006861e-03, Max 6.607888e-02
+%       Opt Version: Ave 8.209630e-02, Min 2.209068e-02, Max 1.474709e-01 
+
+
+% alpha = 0.9:
+%       Non-Opt: Ave 3.382521e-02, Min 2.215611e-03, Max 6.713863e-02
+%       Opt Version: Ave 8.509146e-02, Min 2.389138e-02, Max 1.509780e-01 
+
+
+%% show results
+
+% linear trajectory generation without optimized inverse kinematics
+fprintf('Linear Traj, Non-Opt Control: Ave %d, Min %d, Max %d \n',...
+    ave, minimum, maximum);
+% linear trajectory generation with optimized inverse kinematics
+fprintf('Linear Trak, Opt Control: Ave %d, Min %d, Max %d \n',...
+    ave_opt, minimum_opt, maximum_opt);
+
+
+
+% optimized trajectory generation without optimized inverse kinematics
+fprintf('Opt Traj, Non-Opt Control: Ave %d, Min %d, Max %d \n',...
+    nonLinear_ave, nonLinear_minimum, nonLinear_maximum);
+% optimized trajectory generation with optimized inverse kinematics
+fprintf('Opt: Ave %d, Min %d, Max %d \n',...
+    nonLinear_ave_opt, nonLinear_minimum_opt, nonLinear_maximum_opt);
+
+
+
+
+%% visualize trajectories
+
+% linear path planning
+visualize_trajectory(robot, trajectory, cartesian_pos_final);
+visualize_trajectory(robot, trajectory_opt, cartesian_pos_final);
+
+% optimized path planning
+visualize_trajectory(robot, optTrajectory, cartesian_pos_final);
+visualize_trajectory(robot, optTrajectory_opt, cartesian_pos_final);
+
 
 
