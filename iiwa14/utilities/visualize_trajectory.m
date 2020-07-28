3bab732150b0ac90c8ca50a62f3c8e72c63c7248
Matheer0 <alexqixuan@gmail.com>
2020-07-28 00:02:12 -0700

    change configuration format

--- iiwa14/utilities/visualize_trajectory.m
+++ iiwa14/utilities/visualize_trajectory.m
@@ -5,32 +5,28 @@ function visualize_trajectory (robot, trajectory, destination)
     
     
     % Show the initial configuration of the robot.   
-    position = trajectory.state(1,1:dim_joint) ;
-    config = robot.homeConfiguration;
-    for j = 1 : dim_joint
-        config(j).JointPosition = position(j);
-    end
+    config = trajectory.state(1:dim_joint, 1);
     show(robot, config, 'PreservePlot', false, 'Frames', 'off');
     hold on
     axis([-1 1 -1 1 -0.1 1.5]);
 
-    % Visualize the task-space trajectory. Iterate through the stateTask states and interpolate based on the current time.
+    
+    % Visualize the task-space trajectory, by  
+    % iterating through the stateTask states and interpolate 
     for i = 1 : dim_steps
 
-        position = trajectory.state(i, 1:dim_joint) ;    
-        for j = 1 : dim_joint
-            config(j).JointPosition = position(j);
-        end
+        config = trajectory.state(1:dim_joint, i);    
         show(robot, config, 'PreservePlot', false, 'Frames','off');
         
         % draw a line to show the end-effector trajectory
-        pose_desired = trajectory.hand_pos_desired(i,:) ;
-        pose_current = trajectory.hand_pos_current(i,:) ;
+        pose_desired = trajectory.hand_pos_desired(:, i);
+        pose_current = trajectory.hand_pos_current(:, i);
         plot3(pose_desired(4), pose_desired(5), pose_desired(6), 'r.', 'MarkerSize',20)
         plot3(pose_current(4), pose_current(5), pose_current(6), 'b.', 'MarkerSize',20)
         % draw destination at every step
         plot3(destination(1), destination(2), destination(3), 'g.', 'MarkerSize',20)
         drawnow;
+        
     end
     
     
