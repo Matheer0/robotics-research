5ae9c06c3c4f0c743d5f6c83cec62beb31b3d566
Matheer0 <alexqixuan@gmail.com>
2020-07-29 18:02:44 -0700

    modified ouput format

--- iiwa14/utilities/checkout_result.m
+++ iiwa14/utilities/checkout_result.m
@@ -1,7 +1,9 @@
-function [ave, minimum, maximum] = checkout_result(manipulability_measures)
+function result = checkout_result(manipulability_measures)
     
     ave = mean(manipulability_measures);
     minimum = min(manipulability_measures);
     maximum = max(manipulability_measures);
+    
+    result = [ave; minimum; maximum];
 
 end
